f850f649ccd3b10edc95619ee71dc5d3fb64054e
Meelad <SoheilRavasani@Gmail.com>
2017-07-31 22:16:33 +0430

    1

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Telegraf = require('telegraf')
 const { reply } = Telegraf
 
-const bot = new Telegraf(process.env.359959145:AAFR3XTdCEER06eiCykiGhWhkGo3nUQbCn8)
+const bot = new Telegraf(process.env.'359959145:AAFR3XTdCEER06eiCykiGhWhkGo3nUQbCn8')
 bot.command('/oldschool', (ctx) => ctx.reply('Hello'))
 bot.command('/modern', ({ reply }) => reply('Yo'))
 bot.command('/hipster', reply('λ'))
